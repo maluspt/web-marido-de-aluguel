b74cd6597ba50a62bf721bfc9fd9ee9ef12e7468
Maria Luiza <marialuizacaarvalho@gmail.com>
2020-11-26 12:37:13 -0300

    trt

--- application/src/services/api.ts
+++ application/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://fierce-island-22648.herokuapp.com'
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
